14ff7419ddbc51f1d46df29e095ddc61819ae08b
irusak <irusak@scnsoft.com>
2023-11-27 23:21:39 +0300

    add storage service

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@aws-amplify/ui-react/styles.css';
-import { API } from 'aws-amplify';
+import { Card, Row, Col, Navbar, Container } from 'react-bootstrap';
 import {
   Button,
   Flex,
@@ -17,6 +18,7 @@ import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from './graphql/mutations';
+import { API, Storage } from 'aws-amplify';
 
 const App = ({ signOut, user }) => {
   const [notes, setNotes] = useState([]);
@@ -28,16 +30,28 @@ const App = ({ signOut, user }) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get('image');
     const data = {
       name: form.get('name'),
       description: form.get('description'),
+      image: image.name,
     };
+    if (!!data.image) await Storage.put(data.name, image);
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -46,9 +60,10 @@ const App = ({ signOut, user }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    await Storage.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -57,53 +72,78 @@ const App = ({ signOut, user }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Heading level={1}>{user.username}</Heading>
+          <Button onClick={signOut}>Sign out</Button>
+        </Container>
+      </Navbar>
+      <Container>
+        <Heading level={1}>My Notes App</Heading>
+        <View as="form" margin="3rem 0" onSubmit={createNote}>
+          <Flex direction="row" justifyContent="center">
+            <TextField
+              name="name"
+              placeholder="Note Name"
+              label="Note Name"
+              labelHidden
+              variation="quiet"
+              required
+            />
+            <TextField
+              name="description"
+              placeholder="Note Description"
+              label="Note Description"
+              labelHidden
+              variation="quiet"
+              required
+            />
+            <View
+              name="image"
+              as="input"
+              type="file"
+              style={{ alignSelf: 'end' }}
+            />
+            <Button type="submit" variation="primary">
+              Create Note
             </Button>
           </Flex>
-        ))}
-      </View>
-      <div style={styles.container}>
-        <Heading level={1}>{user.username}</Heading>
-        <Button onClick={signOut}>Sign out</Button>
-      </div>
+        </View>
+        <Heading level={2} marginBottom="2rem">
+          Current Notes
+        </Heading>
+
+        <Row xs={1} md={2} lg={4} xl={6} className="g-4">
+          {notes.map((note) => (
+            <Col key={note.id || note.name}>
+              <Card>
+                {note.image ? (
+                  <Card.Img
+                    style={styles.image}
+                    variant="top"
+                    src={note.image}
+                    alt={`visual aid for ${note.name}`}
+                  />
+                ) : (
+                  <Card.Img
+                    style={styles.image}
+                    variant="top"
+                    src="https://placehold.jp/30/eeeeee/ffffff/300x150.png?text=no+image"
+                    alt={`visual aid for ${note.name}`}
+                  />
+                )}
+                <Card.Body>
+                  <Card.Title>{note.name}</Card.Title>
+                  <Card.Text>{note.description}</Card.Text>
+                  <Button variant="primary" onClick={() => deleteNote(note)}>
+                    Delete note
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </View>
   );
 };
@@ -117,5 +157,6 @@ const styles = {
     alignItems: 'center',
     padding: 20,
   },
+  image: {},
 };
 export default withAuthenticator(App);
